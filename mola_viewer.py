b9554fa15ee237dbce4b15129740921c626f527a
bfunkt <bfunkt@users.noreply.github.com>
2020-06-18 00:47:36 -0700

    Update mola_viewer.py

--- mola_viewer.py
+++ mola_viewer.py
@@ -30,9 +30,10 @@ def run(lat_top=-37.5, lat_bottom=-38.25, long_west=87.5, long_east=88.25, filen
     x_bounds = min(long_west, long_east), max(long_west, long_east)
     y_bounds = lat_bottom, lat_top
 
-    x_range = int(abs((long_east+360) - (long_west+360)) % 360)
-    #x_range = int(max(long_east, long_west) - min(long_east, long_west)) * (10**rnd) + 1)
+    x_range = int(min(abs(long_east-long_west), abs(long_west-long_east)) * (10**rnd) +1)
     y_range = int((lat_top - lat_bottom) * (10**rnd) + 1)
+
+    print('x_range = ', x_range, ' : y_range = ', y_range)
 
     img_coords = np.zeros((y_range, x_range, 2))
     img_coords[:, :, 0] = np.linspace(x_bounds[0], x_bounds[1], x_range)[None, :]
