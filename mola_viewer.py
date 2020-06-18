9d522cc0ca4d056430b1bdcc7268e46025b334a6
bfunkt <bfunkt@users.noreply.github.com>
2020-06-18 00:43:54 -0700

    Smaller csv file

--- mola_viewer.py
+++ mola_viewer.py
@@ -10,7 +10,7 @@ def file_data(path):
         return f.read()
 
 
-def run(lat_top=-35, lat_bottom=-39, long_west=85, long_east=90, filename='PEDR_test.csv', rnd=3):
+def run(lat_top=-37.5, lat_bottom=-38.25, long_west=87.5, long_east=88.25, filename='PEDR_test_2.csv', rnd=4):
     data_coord = []
     data_value = []
 
@@ -30,8 +30,9 @@ def run(lat_top=-35, lat_bottom=-39, long_west=85, long_east=90, filename='PEDR_
     x_bounds = min(long_west, long_east), max(long_west, long_east)
     y_bounds = lat_bottom, lat_top
 
-    x_range = (max(long_east, long_west) - min(long_east, long_west)) * (10**rnd) + 1
-    y_range = (lat_top - lat_bottom) * (10**rnd) + 1
+    x_range = int(abs((long_east+360) - (long_west+360)) % 360)
+    #x_range = int(max(long_east, long_west) - min(long_east, long_west)) * (10**rnd) + 1)
+    y_range = int((lat_top - lat_bottom) * (10**rnd) + 1)
 
     img_coords = np.zeros((y_range, x_range, 2))
     img_coords[:, :, 0] = np.linspace(x_bounds[0], x_bounds[1], x_range)[None, :]
